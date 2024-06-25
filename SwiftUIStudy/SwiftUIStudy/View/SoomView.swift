99126038e0cbf137bcabbe34a8a78e4aecaa995d
sueunal <36671600+sueunal@users.noreply.github.com>
2024-06-25 19:55:14 +0900

    [test]: branch test

--- SwiftUIStudy/SwiftUIStudy/View/SoomView.swift
+++ SwiftUIStudy/SwiftUIStudy/View/SoomView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct SoomView: View {
     var body: some View {
-        Text("Soom")
+        VStack{
+            Text("Soom")
+            Text("Soom")
+            Text("Soom")
+            Text("Soom")
+            Text("Soom")
+            Text("Soom")
+        }
     }
 }
 
