3c4ae2dc7486b02e1466065bed427a3bd1ac64e0
sueunal <36671600+sueunal@users.noreply.github.com>
2024-07-05 22:29:34 +0900

    [feat]: LottoView 생성 및 구현

--- SwiftUIStudy/SwiftUIStudy/View/SoomView.swift
+++ SwiftUIStudy/SwiftUIStudy/View/SoomView.swift
@@ -12,47 +12,50 @@ struct SoomView: View {
     let about: String = "Switf Study"
     @State private var todayLearn: [String] = ["LearningSwift/docs/01.md"]
     var body: some View {
-        ZStack{
-            VStack(alignment: .leading){
-                Spacer()
-                    .frame(height: 32)
-                HStack{
-                    VStack(alignment: .leading){
-                        Text(nickname)
-                            .font(.largeTitle)
-                            .fontWeight(.heavy)
-                            .foregroundStyle(.white)
-                            .italic()
-                        Text(about)
-                            .font(.caption)
-                            .bold()
-                            .foregroundStyle(.gray.opacity(0.8))
-                    }
-                    .padding()
+        NavigationStack{
+            ZStack{
+                VStack(alignment: .leading){
                     Spacer()
-                    Image("Swift")
-                        .resizable()
-                        .frame(width: 50,height: 50)
+                        .frame(height: 32)
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(nickname)
+                                .font(.largeTitle)
+                                .fontWeight(.heavy)
+                                .foregroundStyle(.white)
+                                .italic()
+                            Text(about)
+                                .font(.caption)
+                                .bold()
+                                .foregroundStyle(.gray.opacity(0.8))
+                        }
                         .padding()
+                        Spacer()
+                        Image("Swift")
+                            .resizable()
+                            .frame(width: 50,height: 50)
+                            .padding()
+                    }
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(.indigo.gradient)
+                    )
+                    Spacer()
+                    NavigationLink{
+                        LottoView()
+                    }label: {
+                        Text("다음")
+                            .frame(maxWidth:.infinity)
+                            .frame(height: 50)
+                            .foregroundStyle(.white)
+                            .background(
+                                Circle()
+                                    .fill(.mint.gradient)
+                            )
+                    }
+                    Spacer()
                 }
-                .background(
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(.indigo.gradient)
-                )
                 .padding(.horizontal,10)
-                Section{
-                    Text("TIL(Today I Learn)")
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .font(.title)
-                        .bold()
-                        .padding()
-                }
-                List(todayLearn, id: \.self){ item in
-                    Text("\(item)")
-                        .font(.subheadline)
-                }
-                .listStyle(.inset)
-                Spacer()
             }
         }
     }
