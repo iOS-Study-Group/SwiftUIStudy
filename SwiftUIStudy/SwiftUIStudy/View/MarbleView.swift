e6c0ef8e9aec7640508fd80b3fc4efed4dc72328
SeungWoo <kangsw1025@naver.com>
2024-07-09 10:15:00 +0900

    chore : 뷰 이름 수정 및 파일 분리

--- SwiftUIStudy/SwiftUIStudy/View/MarbleView.swift
+++ SwiftUIStudy/SwiftUIStudy/View/MarbleView.swift
@@ -18,26 +18,32 @@ struct MarbleView: View {
                     NavigationLink {
                         mCalcView()
                     } label: {
-                        VStack(alignment:.leading) {
-                            Text("24.06.18")
-                                .font(.system(size: 16))
-                            Text("계산기")
-                                .font(.system(size: 12))
-                        }
+                        listLabel("24.06.18", "계산기")
                     }
                     NavigationLink {
                         mLottoView()
                     } label: {
-                        VStack(alignment:.leading) {
-                            Text("24.07.02")
-                                .font(.system(size: 16))
-                            Text("로또 번호 생성기")
-                                .font(.system(size: 12))
-                        }
+                        listLabel("24.07.02", "로또 번호 생성기")
+                    }
+                    NavigationLink {
+                        mVoiceView()
+                    } label: {
+                        listLabel("24.07.09", "음성 앱")
                     }
                 }
                 .listStyle(.plain)
             }
+        }
+    }
+}
+
+extension MarbleView {
+    func listLabel(_ date : String, _ title : String) -> some View {
+        VStack(alignment:.leading) {
+            Text(date)
+                .font(.system(size: 16))
+            Text(title)
+                .font(.system(size: 12))
         }
     }
 }
