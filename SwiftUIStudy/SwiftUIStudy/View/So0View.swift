d86a0dec91fed3c00017b69a0d2648e0f0243669
Leesooooyoung <soyoung3466@gmail.com>
2024-07-15 11:36:08 +0900

    feat: 음성메모 기능 추가

--- SwiftUIStudy/SwiftUIStudy/View/So0View.swift
+++ SwiftUIStudy/SwiftUIStudy/View/So0View.swift
@@ -69,12 +69,25 @@ struct So0View: View {
                                 .tint(.white)
                                 
                                 NavigationLink {
-                                    lottoView()
+                                    SLottoView()
                                 } label: {
                                     VStack {
                                         Text("2주차")
                                             .frame(maxWidth: .infinity)
                                         Text("로또 만들기")
+                                            .font(.system(size: 12))
+                                    }
+                                }
+                                .buttonStyle(.borderedProminent)
+                                .tint(.white)
+                                
+                                NavigationLink {
+                                    SRecordView()
+                                } label: {
+                                    VStack {
+                                        Text("3주차")
+                                            .frame(maxWidth: .infinity)
+                                        Text("녹음기 만들기")
                                             .font(.system(size: 12))
                                     }
                                 }
@@ -93,235 +106,6 @@ struct So0View: View {
     }
 }
 
-enum Grades: String {
-    case firstGrade = "1등"
-    case secondGrade = "2등"
-    case thirdGrade = "3등"
-    case forthGrade = "4등"
-    case fifthGrade = "5등"
-    case lose = "낙첨"
-}
-
-struct lottoView:View {
-    @State var numbersArray: [Int] = []
-    @State var autoArray = [Array<Int>]()
-    @State var bonusNum: Int = 0 // 보너스 숫자
-    @State var commonArray: [Int] = []
-    @State var totalPrize: Int = 0
-    @State var resultMessages: [String] = [String]()
-    @State var grades: Set = Set<Int>()
-    @State var isInit: Bool = true
-    @State var prizes: [String:Int] = [Grades.firstGrade.rawValue:2386382421,
-                                       Grades.secondGrade.rawValue:48077302,
-                                       Grades.thirdGrade.rawValue:1404957,
-                                       Grades.forthGrade.rawValue:50000,
-                                       Grades.fifthGrade.rawValue:5000]
-    
-    var body: some View {
-        VStack {
-            Text("\(numbersArray.count != 0 ? "당첨번호" : "")")
-                .font(.title)
-                .bold()
-            HStack {
-                if isInit {
-                    Text("순위별 금액")
-                        .font(.title)
-                        .bold()
-                }
-                if bonusNum != 0 {
-                    ForEach(numbersArray, id: \.self) { num in
-                        makeColoredCircle(num: num)
-                    }
-                    
-                    Text("+")
-                    makeColoredCircle(num: bonusNum)
-                }
-            }
-            .padding()
-            
-            ZStack {
-                Rectangle().fill(Color(UIColor.systemGray6))
-                if isInit {
-                    VStack {
-                        ForEach(prizes.sorted(by: <), id:\.key) { prize in
-                            ZStack {
-                                Capsule()
-                                    .fill(.blue)
-                                    .frame(width: 100, height: 45)
-                                VStack {
-                                    Text(prize.key)
-                                        .foregroundColor(.white)
-                                        .bold()
-                                }
-                            }
-                            Text("\(prize.value)원")
-                                .foregroundColor(.secondary)
-                                    .bold()
-                        }
-                        .padding(5)
-                    }
-                }
-                
-                VStack {
-                    Text("\(numbersArray.count == 0 ? "" : "\(makeResultMessage())")")
-                        .font(.title2)
-                        .bold()
-                        .foregroundStyle(.blue)
-                    Text("\(totalPrize == 0 ? "" : "총 \(totalPrize)원 당첨")")
-                        .font(.title2)
-                        .foregroundColor(.gray)
-                        .bold()
-                }
-                .padding()
-            }
-            
-            VStack {
-                ForEach(autoArray, id: \.self) { array in
-                    HStack {
-                        let intAlpha: Int = autoArray.firstIndex(of: array)!
-                        Text("\(String(format: "%C", intAlpha + 65))")
-                            .frame(width: 40, height: 40)
-                            .bold()
-                            .font(.title3)
-                            .foregroundStyle(.gray)
-                        ForEach(array, id: \.self) { num in
-                            makeColoredCircle(num: num, true)
-                        }
-                        Text("\(resultMessages[autoArray.firstIndex(of: array)!])")
-                            .frame(width: 40, height: 40)
-                            .bold()
-                            .foregroundStyle(.gray)
-                    }
-                }
-                .padding()
-            }
-            
-            Button(action: {
-                resetContent()
-                var numbers: Set = Set<Int>()
-                repeat {
-                    numbers.insert(Int.random(in: 1...45))
-                    if numbers.count == 7 { // 중복되지 않는 보너스 숫자를 위해
-                        numbersArray = numbers.sorted()
-                        bonusNum = numbersArray.last ?? 0
-                        numbersArray.removeLast()
-                    }
-                } while numbers.count < 7
-                
-                autoArray.removeAll()
-                for _ in 0...4 {
-                    var autoNumbers: Set = Set<Int>()
-                    repeat {
-                        autoNumbers.insert(Int.random(in: 1...45))
-                        if autoNumbers.count == 6 {
-                            let sortedArray: Array = autoNumbers.sorted()
-                            autoArray.append(sortedArray)
-                        }
-                    } while autoNumbers.count < 6
-                }
-                makeGrade()
-            }, label: {
-                Text("번호 추첨 시작")
-                    .font(.title2)
-            })
-            .padding()
-            .buttonStyle(.borderedProminent)
-        }
-        .navigationTitle("Lotto")
-        .navigationBarBackButtonHidden(true)
-    }
-    
-    func makeColoredCircle(num: Int, _ isAuto: Bool = false) -> some View {
-        let isContained: Bool = numbersArray.contains(num)
-        let backgroundColor: Color = !isContained || !isAuto ? .clear : .gray
-        var borderColor: Color = .clear
-    
-        switch num {
-        case 1...9:
-            borderColor = isAuto ? .clear : .red
-        case 10...19:
-            borderColor = isAuto ? .clear : .yellow
-        case 20...29:
-            borderColor = isAuto ? .clear : .green
-        case 30...39:
-            borderColor = isAuto ? .clear : .blue
-        default:
-            borderColor = isAuto ? .clear : .gray
-        }
-        
-        let textColor: Color = backgroundColor == .clear ? .black : .white
-        
-        return Circle().stroke(borderColor, lineWidth: 5)
-            .fill(backgroundColor)
-            .frame(width: 40, height: 40)
-            .overlay(
-                Text("\(num)")
-                    .frame(width: 40, height: 40)
-                    .foregroundStyle(textColor)
-                    .bold()
-            )
-    }
-    
-    func makeGrade() {
-        if numbersArray.count == 6 {
-            for i in 0..<autoArray.count {
-                commonArray = autoArray[i].filter{numbersArray.contains($0)}
-                switch commonArray.count {
-                case 6:
-                    totalPrize += prizes[Grades.firstGrade.rawValue] ?? 0
-                    resultMessages.insert(Grades.firstGrade.rawValue, at: i)
-                    grades.insert(1)
-                case 5:
-                    if autoArray[i].contains(bonusNum) {
-                        totalPrize += prizes[Grades.secondGrade.rawValue] ?? 0
-                        resultMessages.insert(Grades.secondGrade.rawValue, at: i)
-                        grades.insert(2)
-                    } else {
-                        totalPrize += prizes[Grades.thirdGrade.rawValue] ?? 0
-                        resultMessages.insert(Grades.thirdGrade.rawValue, at: i)
-                        grades.insert(3)
-                    }
-                case 4:
-                    totalPrize += prizes[Grades.forthGrade.rawValue] ?? 0
-                    resultMessages.insert(Grades.forthGrade.rawValue, at: i)
-                    grades.insert(4)
-                case 3:
-                    totalPrize += prizes[Grades.fifthGrade.rawValue] ?? 0
-                    resultMessages.insert(Grades.fifthGrade.rawValue, at: i)
-                    grades.insert(5)
-                default :
-                    resultMessages.insert(Grades.lose.rawValue, at: i)
-                    grades.insert(0)
-                }
-            }
-        }
-    }
-    
-    func makeResultMessage() -> String {
-        if grades.contains(1) {
-            return "💰🎊🎉1등 축하합니다🎉🎊💰"
-        } else if grades.contains(2) {
-            return "🎊🎉2등 축하합니다🎉🎊"
-        } else if grades.contains(3) {
-            return "🎉3등 축하합니다🎉"
-        } else if grades.contains(4) {
-            return "4등 축하합니다!"
-        } else if grades.contains(5) {
-            return "5등 축하합니다"
-        } else {
-            return "아쉽게도 모두 낙첨입니다"
-        }
-    }
-        
-    func resetContent() {
-        isInit = false
-        bonusNum = 0
-        totalPrize = 0
-        grades.removeAll()
-    }
-}
-
 #Preview {
     So0View()
-//    lottoView()
 }
--- SwiftUIStudy/SwiftUIStudy/View/So0Views/SRecordView.swift
+++ SwiftUIStudy/SwiftUIStudy/View/So0Views/SRecordView.swift
@@ -6,10 +6,209 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct SRecordView: View {
+    @StateObject var audioRecorderManager = AudioRecorderManager()
+    @State var scale: CGFloat = 1.0
+    @State var isAnimate: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                ForEach(audioRecorderManager.recordedFiles, id: \.self) { recordedFile in
+                    Button(action: {
+                        if audioRecorderManager.isRecording {
+                            audioRecorderManager.stopRecording()
+                            isAnimate = false
+                            scale = 1.0
+                        }
+                        
+                        if audioRecorderManager.isPlaying {
+                            audioRecorderManager.stopPlaying()
+                        }
+                        
+                        audioRecorderManager.startPlaying(recordingURL: recordedFile)
+                        
+                    }, label: {
+                        Text(recordedFile.lastPathComponent)
+                            .foregroundStyle(audioRecorderManager.isPlaying && audioRecorderManager.audioPlayer?.url == recordedFile ? .red : .black)
+                    })
+                }
+            }
+            
+            Text(String(format: "%02d:%02d:%02d", audioRecorderManager.hours, audioRecorderManager.minutes, audioRecorderManager.sec))
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(audioRecorderManager.audioLevels, id: \.self) { level in
+                        Capsule()
+                            .frame(width: 2, height: level)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                }
+                .frame(height: 160)
+            }
+            .defaultScrollAnchor(.trailing)
+            .statusBar(hidden: true)
+            .padding()
+                       
+            ZStack {
+                Circle()
+                    .frame(width: 90, height: 90, alignment: .center)
+                    .scaleEffect(scale)
+                    .foregroundStyle(Color(.systemGray3))
+                    .animation(isAnimate ? .easeInOut(duration: 0.5).repeatForever(autoreverses: true) : .default, value: scale)
+                
+                Circle()
+                    .frame(width: 90, height: 90)
+                    .foregroundStyle(Color(.systemGray6))
+                
+                Button {
+                    if audioRecorderManager.isPlaying {
+                        audioRecorderManager.stopPlaying()
+                    }
+                    audioRecorderManager.isRecording ? audioRecorderManager.stopRecording() : audioRecorderManager.startRecording()
+                    isAnimate = isAnimate ? false : true
+                    scale = isAnimate ? 1.1 : 1.0
+                } label: {
+                    Image(systemName: isAnimate ? "mic.fill" : "mic")
+                    
+                }
+                .frame(width: 70, height: 70)
+                .background(.red)
+                .cornerRadius(50)
+                .foregroundStyle(.white)
+            }
+        }
+    }
+}
+
+class AudioRecorderManager: NSObject, ObservableObject {
+    @Published var isPlaying = false
+    @Published var isRecording = false
+    @Published var audioLevels: [CGFloat] = []
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer?
+    var recordTimer: Timer?
+    var timerString: String = ""
+    var recordedFiles = [URL]()
+    let session = AVAudioSession.sharedInstance()
+    var hours: Int = 0, minutes: Int = 0, sec: Int = 0
+    var totalTime: Float = 0.0
+    var timeInterval: TimeInterval = 0.2
+    
+    func startRecording() {
+        do {
+            try session.setCategory(.playAndRecord, mode: .default)
+            try session.setActive(true)
+        } catch {
+            print("session error: \(error.localizedDescription)")
+        }
+        
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd.HH:mm:ss"
+        let nowDate: String = dateFormatter.string(from: Date())
+        let fileURL = getDocumentsDirectory().appendingPathComponent("voice-\(nowDate).m4a")
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: fileURL, settings: settings)
+            audioRecorder?.prepareToRecord()
+            audioRecorder?.isMeteringEnabled = true
+            audioRecorder?.record()
+            isRecording = true
+            audioLevels.removeAll()
+            recordTimer = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: true, block: { (value) in
+                self.getRecordingTime(self.audioRecorder?.currentTime ?? 0.0)
+                if self.audioRecorder?.isRecording ?? false {
+                    self.updateAudioLevels()
+                }
+            })
+        } catch {
+            print("record error: \(error.localizedDescription)")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        recordedFiles.append(audioRecorder!.url)
+        isRecording = false
+        stopTimer()
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        return documentsURL
+    }
+    
+    func startPlaying(recordingURL: URL) {
+        let playSession = AVAudioSession.sharedInstance()
+        do {
+            try playSession.overrideOutputAudioPort(AVAudioSession.PortOverride.speaker)
+        } catch {
+            print("play error: \(error.localizedDescription)")
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: recordingURL)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.isMeteringEnabled = true
+            audioPlayer?.play()
+            isPlaying = true
+            audioLevels.removeAll()
+            totalTime = Float(audioPlayer?.duration ?? 0.0)
+            recordTimer = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: true, block: { value in
+                self.getRecordingTime(self.audioPlayer?.currentTime ?? 0.0)
+                if self.audioPlayer?.isPlaying ?? false {
+                    self.updateAudioLevels()
+                }
+            })
+        } catch {
+            print("open error: \(error.localizedDescription)")
+        }
+    }
+    
+    func stopPlaying() {
+        audioPlayer?.stop()
+        isPlaying = false
+        stopTimer()
+    }
+    
+    func getRecordingTime(_ duration: TimeInterval) {
+        hours = Int(duration / 3600)
+        minutes = Int(duration / 60) % 60
+        sec = Int(duration) % 60
+    }
+    
+    func stopTimer() {
+        hours = 0
+        minutes = 0
+        sec = 0
+        audioLevels.removeAll()
+        recordTimer?.invalidate()
+        recordTimer = nil
+    }
+    
+    func updateAudioLevels() {
+        if isRecording {
+            audioRecorder?.updateMeters()
+            if let averagePower = audioRecorder?.averagePower(forChannel: 0) {
+                audioLevels.append(65 + max(CGFloat(averagePower), -70))
+            }
+        } else if isPlaying {
+            audioPlayer?.updateMeters()
+            if let averagePower = audioPlayer?.averagePower(forChannel: 0) {
+                audioLevels.append(65 + max(CGFloat(averagePower), -70))
+            }
+        }
     }
 }
 
