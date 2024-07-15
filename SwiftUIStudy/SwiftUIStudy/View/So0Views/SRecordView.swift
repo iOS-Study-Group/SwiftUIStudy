b1cf4b848648414e5e96a2d7402aa4377cf842f6
Leesooooyoung <soyoung3466@gmail.com>
2024-07-15 10:52:54 +0900

    feat: 음성메모 기눙 추가

---
+++ SwiftUIStudy/SwiftUIStudy/View/So0Views/SRecordView.swift
@@ -0,0 +1,18 @@
+//
+//  SRecordView.swift
+//  SwiftUIStudy
+//
+//  Created by 이소영 on 7/11/24.
+//
+
+import SwiftUI
+
+struct SRecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SRecordView()
+}
