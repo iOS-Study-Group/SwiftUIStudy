564a29b235fd0c2f49131c58be7ff524d046f7a1
sueun kim <doms@kakao.com>
2024-07-30 23:35:52 +0900

    [del]: sortedData삭제

--- SwiftUIStudy/SwiftUIStudy/View/SoomViews/View/SoomPhotosView.swift
+++ SwiftUIStudy/SwiftUIStudy/View/SoomViews/View/SoomPhotosView.swift
@@ -13,12 +13,14 @@ struct SoomPhotosView: View {
     var body: some View {
         ZStack{
             Color.white.ignoresSafeArea()
+            
             Text("남은 카드: \(viewModel.photos.count) \n드래그 해보세요~")
                 .font(.largeTitle)
                 .foregroundStyle(.black)
                 .animation(.easeInOut)
                 .frame(maxHeight: .infinity,alignment: .top)
                 .padding(.top, 50)
+            
             ForEach(viewModel.photos, id: \.id){ photo in
                 CardView(photo: photo)
                     .rotationEffect(.degrees(Double(photo.id)! * 0.2))
